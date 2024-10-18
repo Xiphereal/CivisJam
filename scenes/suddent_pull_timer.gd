3f8904543940f3b31fa98ab1d52e1c71c2a37bd0
Xiphereal <lozanojalex@gmail.com>
2024-10-18 22:30:11 +0200

    feat: boss suddent pull force is now random between a range

--- scenes/suddent_pull_timer.gd
+++ scenes/suddent_pull_timer.gd
@@ -3,13 +3,12 @@ extends Timer
 @export var min_time = 1
 @export var max_time = 4
 
-@export var force = 5
+@export var min_force = 10
+@export var max_force = 30
 
 func _on_timeout() -> void:
-	%Key.boss_pull(force)
-
+	%Key.boss_pull(randi_range(min_force, max_force))
 	change_timer_randomly()
-	print("timeout!")
 	
 func change_timer_randomly():
 	wait_time = randi_range(min_time, max_time)
