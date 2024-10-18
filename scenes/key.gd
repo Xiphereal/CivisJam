9445ee1f2c928a1dd98ad7a6c195a2536999b366
Xiphereal <lozanojalex@gmail.com>
2024-10-18 20:10:16 +0200

    Reflect if the player has win or lose

--- scenes/key.gd
+++ scenes/key.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var player_pull_force = 5
 @export var boss_pull_force = 1
 
+@onready var did_i_win_or_lose: Label = %DidIWinOrLose
+
 func _process(delta):
 	if Input.is_action_pressed("pull"):
 		position.x -= player_pull_force
@@ -10,10 +12,12 @@ func _process(delta):
 	position.x += boss_pull_force
 
 func _on_win(area: Area2D) -> void:
+	did_i_win_or_lose.text = "You win!" 
 	player_pull_force = 0
 	boss_pull_force = 0
 
 
 func _on_defeat(area: Area2D) -> void:
+	did_i_win_or_lose.text = "You lose..." 
 	player_pull_force = 0
 	boss_pull_force = 0
