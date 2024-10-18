4516d9c8d5a29ad660676c3810413ba5b87a73af
Xiphereal <lozanojalex@gmail.com>
2024-10-18 19:44:58 +0200

    Boss also pulls the key

--- scenes/key.gd
+++ scenes/key.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@export var player_pull_force = 5
+@export var boss_pull_force = 1
+
 func _process(delta):
 	if Input.is_action_pressed("pull"):
-		position.x -= 1
+		position.x -= player_pull_force
+		
+	position.x += boss_pull_force
