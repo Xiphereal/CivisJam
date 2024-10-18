0ad6767a8af88bed737b517c97fd824fa91b2bcf
Xiphereal <lozanojalex@gmail.com>
2024-10-18 20:05:21 +0200

    Player can both win or lose

--- scenes/key.gd
+++ scenes/key.gd
@@ -8,3 +8,12 @@ func _process(delta):
 		position.x -= player_pull_force
 		
 	position.x += boss_pull_force
+
+func _on_win(area: Area2D) -> void:
+	player_pull_force = 0
+	boss_pull_force = 0
+
+
+func _on_defeat(area: Area2D) -> void:
+	player_pull_force = 0
+	boss_pull_force = 0
