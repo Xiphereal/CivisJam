4d2a6bb3bba0d747f12b9fd10c05e3ae11c32f08
Xiphereal <lozanojalex@gmail.com>
2024-10-18 23:03:44 +0200

    feat: kick plays on sudden pull

--- scenes/suddent_pull_timer.gd
+++ scenes/suddent_pull_timer.gd
@@ -6,9 +6,16 @@ extends Timer
 @export var min_force = 10
 @export var max_force = 30
 
+const _KICK = preload("res://audio/KICK.wav")
+
 func _on_timeout() -> void:
 	%Key.boss_pull(randi_range(min_force, max_force))
+	
+	%SFX.stream = _KICK
+	%SFX.play()
+	
 	change_timer_randomly()
+	
 	
 func change_timer_randomly():
 	wait_time = randi_range(min_time, max_time)
