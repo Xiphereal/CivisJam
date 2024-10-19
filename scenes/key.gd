fcc8c457773c3c61c4cfb06acc60da6945ae33ca
Xiphereal <lozanojalex@gmail.com>
2024-10-19 11:15:04 +0200

    feat: boss now pulls harder the further is the key

--- scenes/key.gd
+++ scenes/key.gd
@@ -9,13 +9,18 @@ func _process(delta):
 	if Input.is_action_just_pressed("pull"):
 		player_pull(player_pull_force)
 	
-	boss_pull(boss_pull_force)
+	_boss_constant_pull(delta)
 	
 func boss_pull(force):
 	position.x += force
 
 func player_pull(force):
 	position.x -= player_pull_force
+
+func _boss_constant_pull(delta):
+	var boss_distance_force = %Boss.position.distance_to(position) * 0.1
+	print(boss_distance_force)
+	boss_pull(boss_pull_force * boss_distance_force * delta)
 
 func _on_win(area: Area2D) -> void:
 	did_i_win_or_lose.text = "You win!" 
