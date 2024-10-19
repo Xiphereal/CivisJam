b44b35fb83e4aed221f483ca97116adeadf34206
Xiphereal <lozanojalex@gmail.com>
2024-10-19 13:02:23 +0200

    Added stamina

--- scenes/key.gd
+++ scenes/key.gd
@@ -5,15 +5,23 @@ extends Node2D
 
 @export var boss_pull_force_based_on_distance = 0.3
 
+@export var stamina_boost = 20
+@export var stamina_loss_per_click = 10
+
 @onready var did_i_win_or_lose: Label = %DidIWinOrLose
+
+@onready var stamina := 100
 
 const YOULOSE = preload("res://audio/YOULOSE.wav")
 const YOUWIN = preload("res://audio/YOUWIN.wav")
 
 func _process(delta):
+	%Stamina.text=str(stamina)
 	if Input.is_action_just_pressed("pull"):
-		player_pull(player_pull_force)
-	
+		if stamina > 0:
+			player_pull(player_pull_force)
+			stamina -= stamina_loss_per_click
+			
 	_boss_constant_pull(delta)
 	
 func boss_pull(force):
@@ -23,12 +31,8 @@ func player_pull(force):
 	position.x -= player_pull_force
 
 func _boss_constant_pull(delta):
-	var boss :Node2D = %Boss
-	var position_after_pull :int = position.lerp(
-		boss.position,
-		boss_pull_force_based_on_distance * delta).x
-		
-	position.x = position_after_pull
+	var boss_distance_force = %Boss.position.distance_to(position) * 0.1
+	boss_pull(boss_pull_force * boss_distance_force * delta)
 
 func _on_win(area: Area2D) -> void:
 	did_i_win_or_lose.text = "You win!" 
@@ -45,3 +49,7 @@ func _on_defeat(area: Area2D) -> void:
 func _reach_end_game():
 	player_pull_force = 0
 	boss_pull_force = 0
+
+func _on_stamina_boost_timeout() -> void:
+	if stamina < 100:
+		stamina += stamina_boost 
