31eb76ff048a7fc38ffa499d5a882930c6aa4104
Xiphereal <lozanojalex@gmail.com>
2024-10-19 14:23:26 +0200

    feat: player can "pull" to skip intro

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -1,4 +1,11 @@
 extends VideoStreamPlayer
 
 func _on_finished() -> void:
+	_go_to_fight()
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pull"):
+		_go_to_fight()
+		
+func _go_to_fight():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
