401ee31edc5f759e5c24dfce5da77cbac01101db
Daftjoy <daftjoy@hotmail.com>
2024-10-20 11:59:24 +0200

    Added new sounds for sudden pull

--- scenes/suddent_pull_timer.gd
+++ scenes/suddent_pull_timer.gd
@@ -8,11 +8,20 @@ extends Timer
 
 const _PULL = preload("res://audio/PULL.wav")
 
+const _ROPE1 = preload("res://audio/ROPE 1.wav")
+const _ROPE2 = preload("res://audio/ROPE 2.wav")
+const _ROPE3 = preload("res://audio/ROPE 3.wav")
+
+const sfx_array := [_PULL, _ROPE1, _ROPE2, _ROPE3]
+
+var current_stream = sfx_array[0]
+
 func _on_timeout() -> void:
 	if !%Key.holding:
 		%Key.boss_pull(randi_range(min_force, max_force))
 	
-		%SFX.stream = _PULL
+		current_stream = sfx_array[randi_range(0, 3)]
+		%SFX.stream = current_stream
 		%SFX.play()
 	
 		%Camera.initiate_shake(1)
