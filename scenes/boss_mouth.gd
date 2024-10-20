0812d08101cdb4102c912f235c9870f8d798d598
Daftjoy <daftjoy@hotmail.com>
2024-10-20 13:55:06 +0200

    feat: boss says gotcha

--- scenes/boss_mouth.gd
+++ scenes/boss_mouth.gd
@@ -1,7 +1,12 @@
 extends TextureRect
 
+const BOCADILLO_1 = preload("res://art/bocadillo1.png")
 const BOCADILLO_2 = preload("res://art/bocadillo2.png")
 
 func _on_boss_exhaust_timeout():
 	texture = BOCADILLO_2
+	$AnimationPlayer.play("fade")
+	
+func _say_got_ya():
+	texture = BOCADILLO_1
 	$AnimationPlayer.play("fade")
